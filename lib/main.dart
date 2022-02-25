cd02dacb30f051294021e55277c47d4a8631bb64
siubie <putraprima@gmail.com>
2022-02-25 19:34:00 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Layout());
+}
+
+class Layout extends StatelessWidget {
+  const Layout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Belajar Layout"),
+        ),
+        body: Container(
+          color: Colors.grey,
+          child: Row(
+            children: [
+              BlueBox(),
+              BlueBox(),
+              BlueBox(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  const BlueBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(
+          color: Colors.black,
+          width: 1,
+        ),
+      ),
+    );
+  }
+}
